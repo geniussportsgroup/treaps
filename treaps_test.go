dc30930ccaf53269aa9d0819d9064f4cadfc0724
lrleon <leandro.r.leon@gmail.com>
2020-09-20 09:56:24 -0500

    Put exported methods

--- treaps_test.go
+++ treaps_test.go
@@ -32,7 +32,7 @@ func TestTreap_insert(t *testing.T) {
 	const N = 500
 
 	for i := 0; i < N; i++ {
-		ret := tree.insert(i)
+		ret := tree.Insert(i)
 		assert.NotNil(t, ret)
 	}
 }
@@ -40,7 +40,7 @@ func TestTreap_insert(t *testing.T) {
 func insertNRandomItems(tree *Treap, n int) {
 	for i := 0; i < n; i++ {
 		val := rand.Intn(100 * n)
-		for tree.insert(val) == nil {
+		for tree.Insert(val) == nil {
 			val = rand.Intn(100 * n)
 		}
 	}
@@ -51,10 +51,10 @@ func TestRandomInsertions(t *testing.T) {
 	const N = 500
 	for i := 0; i < N; i++ {
 		val := rand.Intn(100 * N)
-		for tree.insert(val) == nil {
+		for tree.Insert(val) == nil {
 			val = rand.Intn(100 * N)
 		}
-		assert.Equal(t, val, tree.search(val))
+		assert.Equal(t, val, tree.Search(val))
 	}
 
 	assert.Equal(t, N, int(tree.size()))
@@ -67,14 +67,14 @@ func TestTreap_remove(t *testing.T) {
 	insertNRandomItems(tree, N)
 
 	values := make([]int, 0, tree.size())
-	for it := NewIterator(tree); it.hasCurr(); it.next() {
-		values = append(values, it.getCurr().(int))
+	for it := NewIterator(tree); it.HasCurr(); it.Next() {
+		values = append(values, it.GetCurr().(int))
 	}
 
 	for _, val := range values {
-		assert.Equal(t, val, tree.search(val))
-		assert.Equal(t, val, tree.remove(val))
-		assert.Equal(t, nil, tree.search(val))
+		assert.Equal(t, val, tree.Search(val))
+		assert.Equal(t, val, tree.Remove(val))
+		assert.Equal(t, nil, tree.Search(val))
 	}
 }
 
@@ -84,34 +84,34 @@ func TestTreap_split(t *testing.T) {
 	const N = 100
 	insertNRandomItems(tree, N)
 
-	t1, t2 := tree.split(552)
+	t1, t2 := tree.SplitByKey(552)
 
 	assert.Equal(t, N, t1.size()+t2.size())
 	assert.Equal(t, 0, tree.size())
 
-	for it := NewIterator(t1); it.hasCurr(); it.next() {
-		fmt.Print(it.getCurr(), " ")
+	for it := NewIterator(t1); it.HasCurr(); it.Next() {
+		fmt.Print(it.GetCurr(), " ")
 	}
 	fmt.Println()
 
-	for it := NewIterator(t2); it.hasCurr(); it.next() {
-		fmt.Print(it.getCurr(), " ")
+	for it := NewIterator(t2); it.HasCurr(); it.Next() {
+		fmt.Print(it.GetCurr(), " ")
 	}
 	fmt.Println()
 
-	tree.joinExclusive(t1)
+	tree.JoinExclusive(t1)
 
-	for it := NewIterator(tree); it.hasCurr(); it.next() {
-		fmt.Print(it.getCurr(), " ")
+	for it := NewIterator(tree); it.HasCurr(); it.Next() {
+		fmt.Print(it.GetCurr(), " ")
 	}
 	fmt.Println()
 
-	tree.joinExclusive(t2)
+	tree.JoinExclusive(t2)
 	assert.Equal(t, 0, t1.size())
 	assert.Equal(t, 0, t2.size())
 
-	for it := NewIterator(tree); it.hasCurr(); it.next() {
-		fmt.Print(it.getCurr(), " ")
+	for it := NewIterator(tree); it.HasCurr(); it.Next() {
+		fmt.Print(it.GetCurr(), " ")
 	}
 	fmt.Println()
 }
@@ -124,18 +124,18 @@ func TestTreap_searchOrInsert(t *testing.T) {
 
 	for i := 0; i < N; i++ {
 		val := rand.Intn(100 * N)
-		ok, res := tree.searchOrInsert(val)
+		ok, res := tree.SearchOrInsert(val)
 		assert.Equal(t, val, res)
 		if !ok {
-			failures.insert(val)
+			failures.Insert(val)
 		}
 	}
 
 	fmt.Println("tree.size() = ", tree.size())
 	fmt.Println("failures.size() = ", failures.size())
 
-	for it := NewIterator(failures); it.hasCurr(); it.next() {
-		assert.Equal(t, tree.search(it.getCurr()), it.getCurr())
+	for it := NewIterator(failures); it.HasCurr(); it.Next() {
+		assert.Equal(t, tree.Search(it.GetCurr()), it.GetCurr())
 	}
 }
 
@@ -145,9 +145,9 @@ func TestTreap_choose(t *testing.T) {
 	const N = 1000
 	insertNRandomItems(tree, N)
 
-	for i, it := 0, NewIterator(tree); it.hasCurr(); it.next() {
-		item := tree.choose(i)
-		assert.Equal(t, item, it.getCurr())
+	for i, it := 0, NewIterator(tree); it.HasCurr(); it.Next() {
+		item := tree.Choose(i)
+		assert.Equal(t, item, it.GetCurr())
 		i++
 	}
 }
@@ -158,8 +158,8 @@ func TestTreap_rank(t *testing.T) {
 	const N = 1000
 	insertNRandomItems(tree, N)
 
-	for i, it := 0, NewIterator(tree); it.hasCurr(); it.next() {
-		ok, pos := tree.rank(it.getCurr())
+	for i, it := 0, NewIterator(tree); it.HasCurr(); it.Next() {
+		ok, pos := tree.RankInOrder(it.GetCurr())
 		assert.True(t, ok)
 		assert.Equal(t, i, pos)
 		i++
@@ -171,17 +171,17 @@ func TestTreap_splitPos(t *testing.T) {
 	const N = 100
 	insertNRandomItems(tree, N)
 
-	min1, max1, min2, max2 := tree.min(), tree.choose(N/2-1), tree.choose(N/2), tree.max()
+	min1, max1, min2, max2 := tree.Min(), tree.Choose(N/2-1), tree.Choose(N/2), tree.Max()
 
-	t1, t2 := tree.splitPos(N / 2)
+	t1, t2 := tree.SplitByPosition(N / 2)
 
-	for it := NewIterator(t1); it.hasCurr(); it.next() {
-		fmt.Print(it.getCurr(), " ")
+	for it := NewIterator(t1); it.HasCurr(); it.Next() {
+		fmt.Print(it.GetCurr(), " ")
 	}
 	fmt.Println()
 
-	for it := NewIterator(t2); it.hasCurr(); it.next() {
-		fmt.Print(it.getCurr(), " ")
+	for it := NewIterator(t2); it.HasCurr(); it.Next() {
+		fmt.Print(it.GetCurr(), " ")
 	}
 	fmt.Println()
 
@@ -190,10 +190,10 @@ func TestTreap_splitPos(t *testing.T) {
 	assert.NotNil(t, t2)
 	assert.Equal(t, N/2, t1.size())
 	assert.Equal(t, N/2, t2.size())
-	assert.Equal(t, min1, t1.min())
-	assert.Equal(t, max1, t1.max())
-	assert.Equal(t, min2, t2.min())
-	assert.Equal(t, max2, t2.max())
+	assert.Equal(t, min1, t1.Min())
+	assert.Equal(t, max1, t1.Max())
+	assert.Equal(t, min2, t2.Min())
+	assert.Equal(t, max2, t2.Max())
 }
 
 func TestTreap_copy(t *testing.T) {
@@ -205,38 +205,38 @@ func TestTreap_copy(t *testing.T) {
 	assert.True(t, checkTreap(*t1.rootPtr))
 	assert.True(t, checkCounter(*t1.rootPtr))
 
-	t2 := t1.copy()
+	t2 := t1.Copy()
 
-	for it := NewIterator(t1); it.hasCurr(); it.next() {
-		fmt.Print(it.getCurr(), " ")
+	for it := NewIterator(t1); it.HasCurr(); it.Next() {
+		fmt.Print(it.GetCurr(), " ")
 	}
 	fmt.Println()
 
-	for it := NewIterator(t2); it.hasCurr(); it.next() {
-		fmt.Print(it.getCurr(), " ")
+	for it := NewIterator(t2); it.HasCurr(); it.Next() {
+		fmt.Print(it.GetCurr(), " ")
 	}
 	fmt.Println()
 
-	assert.True(t, t1.topologicalEqual(t2))
+	assert.True(t, t1.TopologicalEqual(t2))
 }
 
 func TestTreap_removeRange(t *testing.T) {
 	tree := New(2, cmpInt)
 	const N = 100
 	for i := 0; i < N; i++ {
-		tree.insert(i)
+		tree.Insert(i)
 	}
 
-	midRange := tree.extractRange(40, 60)
+	midRange := tree.ExtractRange(40, 60)
 
-	for key, it := 40, NewIterator(midRange); it.hasCurr(); it.next() {
-		assert.Equal(t, key, it.getCurr())
+	for key, it := 40, NewIterator(midRange); it.HasCurr(); it.Next() {
+		assert.Equal(t, key, it.GetCurr())
 		key++
 	}
 	fmt.Println()
 
-	for it := NewIterator(tree); it.hasCurr(); it.next() {
-		fmt.Print(it.getCurr(), " ")
+	for it := NewIterator(tree); it.HasCurr(); it.Next() {
+		fmt.Print(it.GetCurr(), " ")
 	}
 	fmt.Println()
 
