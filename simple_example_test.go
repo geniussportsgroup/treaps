64040c048fe70f706fccbaeb0e2d53383369aa63
lrleon <leandro.r.leon@gmail.com>
2020-10-15 17:00:34 -0500

    Fixed basic example and improved doc

--- simple_example_test.go
+++ simple_example_test.go
@@ -2,19 +2,18 @@ package treaps
 
 import (
 	"fmt"
-	Set "github.com/geniussportsgroup/treaps"
 	"testing"
 )
 
 func Test_Simple(t *testing.T) {
 
 	// we create a simple tree of 15 integer keys
-	tree := Set.NewTreap(func(i1, i2 interface{}) bool {
+	tree := NewTreap(func(i1, i2 interface{}) bool {
 		return i1.(int) < i2.(int)
 	}, 0, 1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13, 14, 15)
 
 	// Now we traverse the tree with the iterator
-	for it := Set.NewIterator(tree); it.HasCurr(); it.Next() {
+	for it := NewIterator(tree); it.HasCurr(); it.Next() {
 		fmt.Print("[key = ", it.GetCurr())
 		_, pos := tree.RankInOrder(it.GetCurr())
 		fmt.Print(" pos = ", pos, "]")
